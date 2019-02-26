ecdc9291b0141c113884e6e9c3443347b2474bb8
ramonjd <ramonjd@gmail.com>
2019-02-26 18:03:38 +1100

    Avoid rerender of site mockup
    Abstracting utils, removing (maybe) unnecessary files...
    
    added tracking to desktop

--- client/components/signup-site-preview/component.jsx
+++ client/components/signup-site-preview/component.jsx
@@ -9,24 +9,17 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { localize, translate } from 'i18n-calypso';
-import { isEmpty } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import { getIframeSource, getIframePageContent } from 'components/signup-site-preview/utils';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-
-/* TODO @ramonopoly
-	// preload Google fonts
-	// enable theme switching
-	// mobile view
-	// extract util functions
-*/
 function MockupChromeMobile() {
 	return (
 		<div className="signup-site-preview__chrome-mobile">
@@ -54,117 +47,73 @@ function MockupChromeDesktop() {
 	);
 }
 
-// fonts and CSS
-function fvdToFontWeightAndStyle( fvd ) {
-	const weight = fvd[ 1 ] + '00';
-	const style = fvd[ 0 ] === 'i' ? 'italic' : 'normal';
-	return { weight, style };
-}
-
-function getFontCssUri( font ) {
-	const base = 'https://fonts.googleapis.com/css?family=';
-	const variations = font.variations.reduce( ( result, variation ) => {
-		const { weight, style } = fvdToFontWeightAndStyle( variation );
-		const suffix = style === 'italic' ? 'italic' : '';
-		result.push( weight + suffix );
-		return result;
-	}, [] );
-	return `${ base }${ font.id }:${ variations.join( ',' ) }`;
-}
-
-function getIframeCssUri( themeSlug, isRtl ) {
-	return `https://s0.wp.com/wp-content/themes/${ themeSlug }/style${ isRtl ? '-rtl' : '' }.css`;
-}
-
-function getIframeSource( content, isRtl, langSlug, themeSlug, font ) {
-	const source = `
-		<html lang="${ langSlug }" dir="${ isRtl ? 'rtl' : 'ltr' }">
-		<head>
-			<title>${ content.title } – ${ content.tagline }</title>
-			<link type="text/css" media="all" rel="stylesheet" href="https://s0.wp.com/wp-content/plugins/gutenberg-core/build/block-library/style.css" />
-			<link type="text/css" media="all" rel="stylesheet" href="${ getIframeCssUri( themeSlug, isRtl ) }" />
-			<link type="text/css" media="all" rel="stylesheet" href="${ getFontCssUri( font ) }" />
-		</head>
-		<body class="home page-template-default page logged-in">
-			<div id="page" class="site">
-				<header id="masthead" class="site-header">
-					<div class="site-branding-container">
-						<div class="site-branding">
-							<p class="site-title"><a href="#" rel="home">${ content.title }</a></p>
-							<p class="site-description"><a href="#" rel="home">${ content.tagline }</a></p>
-							<nav id="site-navigation" class="main-navigation" aria-label="Top Menu"></nav>
-						</div>
-					</div>
-				</header>
-				<div id="content" class="site-content">
-					<section id="primary" class="content-area">
-						<main id="main" class="site-main">
-							<article class="page type-page status-publish hentry entry">
-								<div class="entry-content">${ content.body }</div>
-							</article>
-						</main>
-					</section>
-				</div>
-			</div>
-		</body>
-	</html>`;
-
-	return URL.createObjectURL( new Blob( [ source ], { type: 'text/html' } ) )
-}
-
 export class SignupSitePreview extends Component {
 	static propTypes = {
 		// The viewport device to show initially
 		defaultViewportDevice: PropTypes.oneOf( [ 'desktop', 'phone' ] ),
 		isRtl: PropTypes.bool,
 		langSlug: PropTypes.string,
-		themeSlug: PropTypes.string,
-		siteStyle: PropTypes.string,
-		siteType: PropTypes.string,
+		cssUrl: PropTypes.string,
+		fontUrl: PropTypes.string,
 		// Iframe body content
 		content: PropTypes.object,
+		onPreviewClick: PropTypes.func,
 	};
 
 	static defaultProps = {
 		defaultViewportDevice: 'desktop',
 		isRtl: false,
 		langSlug: 'en',
-		siteStyle: 'default',
-		siteType: 'business',
-		themeSlug: 'pub/professional-business',
 		content: {},
+		onPreviewClick: () => {},
 	};
 
 	constructor( props ) {
 		super( props );
 		this.iframe = React.createRef();
-		this.state = {
-			loaded: false,
-		};
+	}
+
+	componentDidMount() {
+		this.setIframeSource();
 	}
 
 	shouldComponentUpdate( nextProps ) {
+		if (
+			this.props.cssUrl !== nextProps.cssUrl ||
+			this.props.fontUrl !== nextProps.fontUrl ||
+			this.props.langSlug !== nextProps.langSlug
+		) {
+			this.setIframeSource();
+			return false;
+		}
+
 		if ( this.props.content.title !== nextProps.content.title ) {
-			this.setIframeContent( '.site-title > a', nextProps.content.title );
+			this.setIframeContent( '.site-builder__title', nextProps.content.title );
+			return false;
 		}
 
 		if ( this.props.content.tagline !== nextProps.content.tagline ) {
-			this.setIframeContent( '.site-description > a', nextProps.content.tagline );
-		}
-
-		if ( this.props.themeSlug !== nextProps.themeSlug ) {
-			return true;
-		}
-
-		if ( this.props.siteStyle !== nextProps.siteStyle ) {
-			return true;
+			this.setIframeContent( '.site-builder__description', nextProps.content.tagline );
+			return false;
 		}
 
 		if ( this.props.content.body !== nextProps.content.body ) {
-			this.setIframeContent( '.entry-content', nextProps.content.body );
+			this.setIframePageContent( nextProps.content );
+			return false;
 		}
 
 		return false;
+	}
+
+	setIframePageContent( content ) {
+		if ( ! this.iframe.current ) {
+			return;
+		}
+		const element = this.iframe.current.contentWindow.document.querySelector( '.entry' );
+
+		if ( element ) {
+			element.innerHTML = getIframePageContent( content );
+		}
 	}
 
 	setIframeContent( selector, content ) {
@@ -172,17 +121,53 @@ export class SignupSitePreview extends Component {
 			return;
 		}
 		const element = this.iframe.current.contentWindow.document.querySelector( selector );
+
 		if ( element ) {
 			element.innerHTML = content;
 		}
 	}
 
+	setOnPreviewClick = () => {
+		if ( ! this.iframe.current ) {
+			return;
+		}
+
+		this.iframe.current.contentWindow.document.querySelector( '.home' ).onclick = () =>
+			this.props.onPreviewClick( this.props.defaultViewportDevice );
+	};
+
+	setIframeIsLoading = () => {
+		if ( ! this.iframe.current ) {
+			return;
+		}
+
+		this.iframe.current.contentWindow.document
+			.querySelector( '.home' )
+			.classList.remove( 'is-loading' );
+	};
+
+	setLoaded = () => {
+		this.setOnPreviewClick();
+		this.setIframeIsLoading();
+	};
+
+	setIframeSource = () => {
+		if ( ! this.iframe.current ) {
+			return;
+		}
+		const { cssUrl, fontUrl, content, isRtl, langSlug } = this.props;
+
+		// For memory management: https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL
+		URL.revokeObjectURL( this.iframe.current.src );
+
+		this.iframe.current.src = getIframeSource( content, cssUrl, fontUrl, isRtl, langSlug );
+	};
+
 	render() {
-		const { font, isDesktop, isPhone, content, isRtl, langSlug, themeSlug } = this.props;
+		const { content, isDesktop, isPhone } = this.props;
 		const className = classNames( this.props.className, 'signup-site-preview__wrapper', {
 			'is-desktop': isDesktop,
 			'is-phone': isPhone,
-			'is-empty': isEmpty( content.body ),
 		} );
 
 		return (
@@ -192,7 +177,6 @@ export class SignupSitePreview extends Component {
 					<iframe
 						ref={ this.iframe }
 						className="signup-site-preview__iframe"
-						src={ getIframeSource( content, isRtl, langSlug, themeSlug, font ) }
 						title={ `${ content.title } – ${ content.tagline }` }
 						onLoad={ this.setLoaded }
 					/>
@@ -209,5 +193,3 @@ export default connect(
 	} ),
 	null
 )( localize( SignupSitePreview ) );
-
-
--- client/signup/site-mockup/index.jsx
+++ client/signup/site-mockup/index.jsx
@@ -2,31 +2,46 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import Gridicon from 'gridicons';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import classNames from 'classnames';
 import { each, find, isEmpty } from 'lodash';
 import { translate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import SignupSitePreview from 'components/signup-site-preview';
 import { getSiteType } from 'state/signup/steps/site-type/selectors';
 import {
-	getSiteVerticalName,
 	getSiteVerticalPreview,
+	getSiteVerticalSlug,
 } from 'state/signup/steps/site-vertical/selectors';
 import { getSiteInformation } from 'state/signup/steps/site-information/selectors';
 import { getSiteStyle } from 'state/signup/steps/site-style/selectors';
-import SignupSitePreview from 'components/signup-site-preview'
-import Gridicon from 'gridicons';
-import { getSiteStyleOptions } from 'lib/signup/site-styles';
+import { getSiteStyleOptions, getThemeCssUri } from 'lib/signup/site-styles';
+import { recordTracksEvent } from 'state/analytics/actions';
+import { getLocaleSlug, getLanguage } from 'lib/i18n-utils';
 
 /**
  * Style dependencies
  */
 import './style.scss';
+
+function SiteMockupHelpTip() {
+	return (
+		<div className="site-mockup__help-tip">
+			<p>
+				{ translate(
+					'Scroll down to see your website. Once you complete setup you’ll be able to customize it further.'
+				) }
+			</p>
+			<Gridicon icon="chevron-down" />
+		</div>
+	);
+}
 
 class SiteMockups extends Component {
 	static propTypes = {
@@ -34,6 +49,7 @@ class SiteMockups extends Component {
 		phone: PropTypes.string,
 		siteStyle: PropTypes.string,
 		siteType: PropTypes.string,
+		stepName: PropTypes.string,
 		title: PropTypes.string,
 		vertical: PropTypes.string,
 		verticalPreviewContent: PropTypes.string,
@@ -44,6 +60,7 @@ class SiteMockups extends Component {
 		phone: '',
 		siteStyle: '',
 		siteType: '',
+		stepName: '',
 		title: '',
 		vertical: '',
 		verticalPreviewContent: '',
@@ -97,59 +114,80 @@ class SiteMockups extends Component {
 		return parts.slice( 0, 2 ).join( ', ' );
 	}
 
+	handleClick = size =>
+		this.props.handleClick( this.props.verticalSlug, this.props.siteStyle, size );
+
 	render() {
-		const { font, siteStyle, siteType, title, themeSlug, verticalName, verticalPreviewContent } = this.props;
-		const siteMockupClasses = classNames( {
-			'site-mockup__wrap': true,
+		const { fontUrl, shouldShowHelpTip, title, themeSlug, verticalPreviewContent } = this.props;
+
+		const siteMockupClasses = classNames( 'site-mockup__wrap', {
 			'is-empty': isEmpty( verticalPreviewContent ),
 		} );
+		const langSlug = getLocaleSlug();
+		const language = getLanguage( langSlug );
+		const isRtl = language && language.rtl;
 		const otherProps = {
-			font,
+			fontUrl,
+			cssUrl: getThemeCssUri( themeSlug, isRtl ),
 			content: {
 				title,
 				tagline: this.getTagline(),
 				body: this.getContent( verticalPreviewContent ),
 			},
-			siteType,
-			siteStyle,
-			verticalName,
-			themeSlug,
+			langSlug,
+			isRtl,
 		};
 
 		return (
 			<div className={ siteMockupClasses }>
-				<div className="site-mockup__help-tip">
-					<p>
-						{ translate(
-							'Scroll down to see your website. Once you complete setup you’ll be able to customize it further.'
-						) }
-					</p>
-					<Gridicon icon="chevron-down" />
+				{ shouldShowHelpTip && <SiteMockupHelpTip /> }
+				<div className="site-mockup__devices">
+					<SignupSitePreview
+						defaultViewportDevice="desktop"
+						{ ...otherProps }
+						onPreviewClick={ this.handleClick }
+					/>
+					<SignupSitePreview
+						defaultViewportDevice="phone"
+						{ ...otherProps }
+						onPreviewClick={ this.handleClick }
+					/>
 				</div>
-				<SignupSitePreview defaultViewportDevice="desktop" { ...otherProps } />
-				<SignupSitePreview defaultViewportDevice="phone" { ...otherProps } />			</div>
+			</div>
 		);
 	}
 }
 
-export default connect( state => {
-	const siteInformation = getSiteInformation( state );
-	const siteStyle = getSiteStyle( state );
-	const siteType = getSiteType( state );
-	const styleOptions = getSiteStyleOptions( siteType );
-	const style = find( styleOptions, { id: siteStyle || 'default' } );
-	return {
-		title: siteInformation.title || translate( 'Your New Website' ),
-		address: siteInformation.address,
-		phone: siteInformation.phone,
-		siteStyle,
-		siteType,
-		verticalName: getSiteVerticalName( state ),
-		verticalPreviewContent: getSiteVerticalPreview( state ),
-		themeSlug: style.theme,
-		font: {
-			...style.font,
-			id: style.font.name.trim().replace( / /g, '+' ),
-		},
-	};
-} )( SiteMockups );
+export default connect(
+	( state, ownProps ) => {
+		const siteInformation = getSiteInformation( state );
+		const siteStyle = getSiteStyle( state );
+		const siteType = getSiteType( state );
+		const styleOptions = getSiteStyleOptions( siteType );
+		const style = find( styleOptions, { id: siteStyle || 'professional' } );
+		return {
+			title: siteInformation.title || translate( 'Your New Website' ),
+			address: siteInformation.address,
+			phone: siteInformation.phone,
+			siteStyle,
+			siteType,
+			verticalPreviewContent: getSiteVerticalPreview( state ),
+			verticalSlug: getSiteVerticalSlug( state ),
+			shouldShowHelpTip:
+				'site-topic-with-preview' === ownProps.stepName ||
+				'site-information-title-with-preview' === ownProps.stepName,
+			themeSlug: style.theme,
+			fontUrl: style.fontUrl,
+		};
+	},
+	dispatch => ( {
+		handleClick: ( verticalSlug, siteStyle, size ) =>
+			dispatch(
+				recordTracksEvent( 'calypso_signup_site_preview_mockup_clicked', {
+					size,
+					vertical_slug: verticalSlug,
+					site_style: siteStyle || 'default',
+				} )
+			),
+	} )
+)( SiteMockups );
